0d8956cb1307836dc50a6c6de1023750579de3c0
zhangsan <zhangsan@163.com>
2021-04-01 08:44:05 +0800

    第一次提交

---
+++ git-demo/src/main/java/com/zcwl/git/GitHello.java
@@ -0,0 +1,10 @@
+package com.zcwl.git;
+
+public class GitHello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
